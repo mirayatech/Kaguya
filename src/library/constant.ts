f0d2af42edab4db78fcb84ad7527380fa4fbe41d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-30 16:27:50 +0100

    feat: create all required constant

---
+++ src/library/constant.ts
@@ -0,0 +1,6 @@
+export const CURRENT_SEASON_URL = 'https://api.jikan.moe/v4/seasons/now'
+export const TOP_ANIME_URL = 'https://api.jikan.moe/v4/top/anime'
+export const TOP_MANGA_URL = 'https://api.jikan.moe/v4/top/manga'
+export const TOP_CHARACTERS_URL = 'https://api.jikan.moe/v4/top/characters'
+export const ANIME_GENRES_URL = 'https://api.jikan.moe/v4/genres/anime'
+export const MANGA_GENRES_URL = 'https://api.jikan.moe/v4/genres/manga'
