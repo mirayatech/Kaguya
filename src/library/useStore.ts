7bdf11ebe77e018ce988826bc14345a93f01227d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-30 16:28:42 +0100

    feat: state managment for web status

---
+++ src/library/useStore.ts
@@ -0,0 +1,29 @@
+import type { User } from 'firebase/auth'
+
+import create from 'zustand'
+
+type StoreType = {
+  currentUser: undefined | null | User
+  setCurrentUser: (user: User | null) => void
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const useUserStore = create<StoreType>((set: any) => ({
+  currentUser: undefined,
+  setCurrentUser: (user) => set({ currentUser: user }),
+}))
+
+export type Status = 'idle' | 'loading' | 'success' | 'error'
+
+type LoadingState = {
+  status: Status
+  setStatus: (status: Status) => void
+}
+
+export const useLoadingStore = create<LoadingState>((set) => ({
+  status: 'idle',
+  setStatus: (status: Status) =>
+    set(() => ({
+      status: status,
+    })),
+}))
