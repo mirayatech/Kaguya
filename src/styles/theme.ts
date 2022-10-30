bb5e276889da5d694272b5b96498678fcb8e98ac
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-30 18:15:30 +0100

    feat: theme system done

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  White: '#fff',
+  DrakGrey: '#1A1A1A',
+  MediumGrey: '#262626',
+  HoverDarkGrey: '#313131',
+  Dot: '#616161',
+  Grey: '#d1d5db',
+  GreyHover: '#f3f4f6',
+  Green: '#2DCB70',
+  Red: '#F75050',
+}
