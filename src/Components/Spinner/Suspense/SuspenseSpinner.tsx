2fb204c7a7bdf2ac94a9ea47aa36387faf8d11e5
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-31 11:23:42 +0100

    feat: creating a loaidng spinnerand suspense

--- src/Components/Spinner/Suspense/SuspenseSpinner.tsx
+++ src/Components/Spinner/Suspense/SuspenseSpinner.tsx
@@ -1,13 +1,11 @@
 import { ImSpinner8 } from 'react-icons/im'
-import { useStore } from '../../../library'
 
 import '../style.css'
 
-export const LoadingSpinner = () => {
-  const { status } = useStore()
-  return status === 'loading' ? (
+export const SuspenseSpinner = () => {
+  return (
     <div role="alert" aria-label="loading" className="spinner">
       <ImSpinner8 className="spin" />
     </div>
-  ) : null
+  )
 }
