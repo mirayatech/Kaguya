087f73aee0919c5756886471f4f5c2b44d3b5eb0
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-31 11:23:12 +0100

    feat: hook to create user

---
+++ src/hooks/useCreateUserWithEmailAndPassword.tsx
@@ -0,0 +1,42 @@
+import { createUserWithEmailAndPassword as createUserWithEmailAndPasswordAuth } from '@firebase/auth'
+import { doc, setDoc } from '@firebase/firestore'
+import { firebaseAuth, firebaseDb } from '../library'
+import { FirebaseError } from 'firebase/app'
+import { useState } from 'react'
+
+export const useCreateUserWithEmailAndPassword = () => {
+  const [signUpError, setSignUpError] = useState<FirebaseError | null>(null)
+
+  const createUserWithEmailAndPassword = async (
+    email: string,
+    password: string
+  ) => {
+    try {
+      const user = await createUserWithEmailAndPasswordAuth(
+        firebaseAuth,
+        email,
+        password
+      )
+
+      const userRef = doc(firebaseDb, `users/${firebaseAuth.currentUser?.uid}`)
+
+      setDoc(userRef, {
+        name: '',
+        email: firebaseAuth.currentUser?.email,
+        avatarUrl: '',
+        bookmark: [],
+        favorites: [],
+      })
+    } catch (error) {
+      setSignUpError(error as FirebaseError)
+      setTimeout(() => {
+        setSignUpError(null)
+      }, 3000)
+    }
+  }
+
+  return {
+    createUserWithEmailAndPassword,
+    signUpError,
+  }
+}
