88891708d3945e38e4cd91d7238c4695dd68bcbc
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-01 07:43:43 +0100

    feat: generate the current type

--- src/library/type.ts
+++ src/library/type.ts
@@ -5,3 +5,25 @@ export type UserType = {
   favorites: []
   name: string
 }
+
+export type AnimeType = {
+  title: string
+  mal_id: string
+  favorites: number
+  score: number
+  synopsis: string
+  status: string
+  genres: {
+    mal_id: number
+    name: string
+  }
+
+  images: {
+    jpg: {
+      large_image_url: string
+    }
+    webp: {
+      large_image_url: string
+    }
+  }
+}
