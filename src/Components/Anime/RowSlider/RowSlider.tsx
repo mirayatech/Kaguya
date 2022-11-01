9352ddc7524d07aea7faf6c8508d071e3aedcb1c
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-01 09:00:35 +0100

    feat: fetching anime row-slider's data and render anime poster

--- src/Components/Anime/RowSlider/RowSlider.tsx
+++ src/Components/Anime/RowSlider/RowSlider.tsx
@@ -1,3 +1,18 @@
-export function RowSlider() {
-  return <div>HeadlessSwiper</div>
+import { AnimeType } from '../../../library'
+
+type RowSliderProps = {
+  anime: AnimeType
+  setAnimeInfo: React.Dispatch<React.SetStateAction<AnimeType | undefined>>
+}
+
+export function RowSlider({ anime, setAnimeInfo }: RowSliderProps) {
+  return (
+    <div>
+      <img
+        src={anime.images.jpg.large_image_url}
+        alt=""
+        onClick={() => setAnimeInfo(anime)}
+      />
+    </div>
+  )
 }
