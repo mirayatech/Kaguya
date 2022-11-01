5f8bcbac283a5728c5be7f4e4859a1142ebe3c78
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-01 10:04:47 +0100

    style: add styling to home banner

--- src/Components/Anime/HomeBanner/Homebanner.tsx
+++ src/Components/Anime/HomeBanner/Homebanner.tsx
@@ -2,12 +2,35 @@ import { AnimeType } from '../../../library'
 
 import { FaRegSmile } from 'react-icons/fa'
 import { HiHeart } from 'react-icons/hi'
-import { Banner, TopGardient } from './style'
+import {
+  Banner,
+  TopGardient,
+  AnimeInfo,
+  Title,
+  Wrapper,
+  MiniWrapper,
+  Summary,
+} from './style'
+
+import { BsDot } from 'react-icons/bs'
+
+import './style.css'
 
 type HomebannerProps = {
   animeInfo: AnimeType | undefined
 }
 export function Homebanner({ animeInfo }: HomebannerProps) {
+  const percentage = animeInfo?.score
+  const score = Math.floor((percentage as number) * 10)
+
+  const truncateString = (str: string, num: number) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + '...'
+    } else {
+      return str
+    }
+  }
+
   return (
     <Banner
       style={{
@@ -15,22 +38,38 @@ export function Homebanner({ animeInfo }: HomebannerProps) {
       }}
     >
       <TopGardient />
-      <div>
-        {animeInfo?.title}
-        <div>
-          <p>
-            <FaRegSmile /> {animeInfo?.score}{' '}
-          </p>
-          <p>
-            <HiHeart /> {animeInfo?.favorites}
-          </p>
-          <ul>
-            {animeInfo?.genres.map((genre) => (
-              <li key={genre.mal_id}>{genre.name}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
+
+      <AnimeInfo>
+        <Wrapper>
+          <Title>{animeInfo?.title}</Title>
+
+          <MiniWrapper>
+            <p>
+              <FaRegSmile className="smile__svg" /> {score}&#37;
+            </p>
+            <p>
+              <HiHeart className="heart__svg" /> {animeInfo?.favorites}
+            </p>
+
+            <ul>
+              {animeInfo?.genres.map((genre) => (
+                <li key={genre.mal_id}>
+                  {genre.name} <BsDot className="dot" />
+                </li>
+              ))}
+
+              {animeInfo?.themes.map((theme) => (
+                <li key={theme.mal_id}>
+                  {theme.name} <BsDot className="dot" />
+                </li>
+              ))}
+            </ul>
+          </MiniWrapper>
+          <Summary>
+            {truncateString(animeInfo?.synopsis as string, 200)}
+          </Summary>
+        </Wrapper>
+      </AnimeInfo>
     </Banner>
   )
 }
--- src/Components/Anime/HomeBanner/style.tsx
+++ src/Components/Anime/HomeBanner/style.tsx
@@ -5,7 +5,7 @@ export const TopGardient = styled.div`
   bottom: top;
   height: 15vh;
   position: absolute;
-
+  align-self: flex-start;
   background-image: linear-gradient(
     0deg,
     transparent,
@@ -17,6 +17,48 @@ export const TopGardient = styled.div`
 
 export const Banner = styled.div`
   height: 60vh;
+  margin: auto;
+  display: flex;
+  position: relative;
+  align-items: center;
   background-size: cover;
+  background-position: center;
   background-repeat: no-repeat;
+`
+
+export const AnimeInfo = styled.div`
+  width: 100%;
+  margin: auto;
+  padding: 0 30px;
+`
+
+export const Wrapper = styled.div`
+  margin: auto;
+  display: flex;
+  max-width: 2200px;
+  flex-direction: column;
+`
+
+export const Title = styled.h1`
+  margin-bottom: 15px;
+  text-transform: uppercase;
+  font-size: calc(36 / 16 * 1rem);
+`
+
+export const MiniWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+
+  p {
+    display: flex;
+    font-weight: 500;
+    align-items: center;
+    font-size: calc(18 / 16 * 1rem);
+  }
+`
+
+export const Summary = styled.p`
+  color: #d1d5db;
+  max-width: 800px;
 `
