c358efe2708f3bd1ba2e71f6d7a5400c3ff5e2c3
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-01 17:55:54 +0100

    feat: creating favotire component and info

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ export function App() {
   const SignIn = lazy(() => import('./pages/SignIn'))
   const SignUp = lazy(() => import('./pages/SignUp'))
   const CreateAccount = lazy(() => import('./pages/CreateAccount'))
+  const Favorite = lazy(() => import('./pages/Favorites/'))
 
   return (
     <AuthContextProvider>
@@ -45,6 +46,14 @@ export function App() {
           element={
             <Suspense fallback={<SuspenseSpinner />}>
               <SignUp />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <Suspense fallback={<SuspenseSpinner />}>
+              <Favorite />
             </Suspense>
           }
         />
---
+++ src/pages/Favorites/index.ts
@@ -0,0 +1,4 @@
+export { default } from './Favorite'
+
+export * from './Favorites'
+export * from './Info'
