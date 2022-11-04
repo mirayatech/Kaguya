a4577ededa22a1b5b60ec7b3484300130a49008d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-04 07:27:17 +0100

    fix: remove 2 urls (they do not work)

--- src/library/constant.ts
+++ src/library/constant.ts
@@ -4,7 +4,6 @@ export const ANIME_GENRES_URL = 'https://api.jikan.moe/v4/genres/anime'
 export const UPCOMING_ANIME = 'https://api.jikan.moe/v4/seasons/upcoming'
 export const RECOMMENDATION_ANIME =
   'https://api.jikan.moe/v4/recommendations/anime'
-export const RECENT_ANIME_REVIEWS = 'https://api.jikan.moe/v4/reviews/anime'
 
 export const TOP_MANGA_URL = 'https://api.jikan.moe/v4/top/manga'
 export const RANDOM_MANGA = 'https://api.jikan.moe/v4/random/manga'
@@ -12,7 +11,6 @@ export const MANGA_GENRES_URL = 'https://api.jikan.moe/v4/genres/manga'
 export const UPCOMING_MANGA = 'https://api.jikan.moe/v4/reviews/manga'
 export const RECOMMENDATION_MANGA =
   'https://api.jikan.moe/v4/recommendations/manga'
-export const RECENT_MANGA_REVIEWS = 'https://api.jikan.moe/v4/reviews/manga'
 
 export const CURRENT_SEASON_URL = 'https://api.jikan.moe/v4/seasons/now'
 export const TOP_CHARACTERS_URL = 'https://api.jikan.moe/v4/top/characters'
