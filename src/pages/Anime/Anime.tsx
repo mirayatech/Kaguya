3adc2535417a9040b7ad9d2012dd6b0ba28809fc
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-05 18:14:34 +0100

    feat: create primary/Top section and sdisplaying its data

--- src/pages/Anime/Anime.tsx
+++ src/pages/Anime/Anime.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AnimeType } from '../../library'
-import { Banner } from './style'
+import { Banner, Section } from './style'
 
 export default function Anime() {
   const [anime, setAnime] = useState<AnimeType | null>(null)
@@ -21,5 +21,62 @@ export default function Anime() {
     getAnimeInformation()
   }, [])
 
-  return <></>
+  const PERCENTAGE = anime?.score
+  const SCORE = Math.floor((PERCENTAGE as number) * 10)
+
+  const ANIME_DURATION = anime?.duration
+  const DURATION = ANIME_DURATION?.split(' ').slice(0, 1).join(' ')
+
+  // 👇️ First 2 words
+
+  return (
+    <>
+      {anime ? (
+        <div>
+          <Banner
+            style={{
+              backgroundImage: `linear-gradient(to bottom, #000000e0, #00000020), url(${anime?.trailer.images.maximum_image_url}) `,
+            }}
+          />
+
+          <Section>
+            <img src={anime.images.jpg.large_image_url} alt="" />
+            <div>
+              <button>Watch Trailer</button>
+              <h1>{anime.title}</h1>
+
+              <ul>
+                {anime.genres.map((genre) => (
+                  <li key={genre.mal_id}>{genre.name}</li>
+                ))}
+              </ul>
+              <p>{anime.synopsis}</p>
+
+              <div>
+                <div>
+                  <p>Score</p>
+                  <p>{SCORE}&#37;</p>
+                </div>
+
+                <div>
+                  <p>Total episodes</p>
+                  <p>{anime.episodes}</p>
+                </div>
+                <div>
+                  <p>Duration</p>
+                  <p>{DURATION} minutes</p>
+                </div>
+                <div>
+                  <p>Status</p>
+                  <p>{anime.status}</p>
+                </div>
+              </div>
+            </div>
+          </Section>
+        </div>
+      ) : (
+        ''
+      )}
+    </>
+  )
 }
