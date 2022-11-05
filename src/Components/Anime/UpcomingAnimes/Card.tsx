dd79100623c82df83152b9a94aceaa57ca575f1d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-05 09:02:32 +0100

    feat: rendering/displaying upcoming anime data

--- src/Components/Anime/UpcomingAnimes/Card.tsx
+++ src/Components/Anime/UpcomingAnimes/Card.tsx
@@ -5,5 +5,25 @@ type CardProps = {
 }
 
 export function Card({ anime }: CardProps) {
-  return <div>Card</div>
+  return (
+    <div>
+      <div>
+        <div>
+          <img src={anime.images.jpg.large_image_url} alt="" />
+        </div>
+        <div>
+          <p>{anime.title}</p>
+
+          <ul>
+            {anime.genres.map((genre) => (
+              <li key={genre.mal_id}>{genre.name}</li>
+            ))}
+          </ul>
+          <p>{anime.favorites}</p>
+        </div>
+      </div>
+
+      <p>{anime.title}</p>
+    </div>
+  )
 }
