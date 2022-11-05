bbef9d49caacb6e58e54c749e00216b7ddae81a1
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-05 19:27:28 +0100

    atyle: add styl;ing to primary/Top section

--- src/pages/Anime/Anime.tsx
+++ src/pages/Anime/Anime.tsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AnimeType } from '../../library'
-import { Banner, Section } from './style'
+import { RiPlayFill } from 'react-icons/ri'
+
+import {
+  Banner,
+  Poster,
+  Section,
+  Button,
+  AnimeInfo,
+  AnimeShowcase,
+} from './style'
+import { BsDot } from 'react-icons/bs'
 
 export default function Anime() {
   const [anime, setAnime] = useState<AnimeType | null>(null)
 
   const { id } = useParams()
 
-  const URL = `https://api.jikan.moe/v4/anime/${id}/full`
+  const URL = `https://api.jikan.moe/v4/anime/${id}`
 
   const getAnimeInformation = async () => {
     const response = await fetch(URL)
@@ -32,48 +42,102 @@ export default function Anime() {
   return (
     <>
       {anime ? (
-        <div>
-          <Banner
-            style={{
-              backgroundImage: `linear-gradient(to bottom, #000000e0, #00000020), url(${anime?.trailer.images.maximum_image_url}) `,
-            }}
-          />
+        <>
+          <div>
+            <Banner
+              style={{
+                backgroundImage: `linear-gradient(to bottom, #000000e0, #00000020), url(${anime?.trailer.images.maximum_image_url}) `,
+              }}
+            />
 
-          <Section>
-            <img src={anime.images.jpg.large_image_url} alt="" />
-            <div>
-              <button>Watch Trailer</button>
-              <h1>{anime.title}</h1>
+            <Section>
+              <Poster src={anime.images.jpg.large_image_url} alt="" />
+              <AnimeInfo>
+                <Button>
+                  <RiPlayFill className="play__svg" /> Watch Trailer
+                </Button>
+                <h1>{anime.title}</h1>
 
-              <ul>
-                {anime.genres.map((genre) => (
-                  <li key={genre.mal_id}>{genre.name}</li>
-                ))}
-              </ul>
-              <p>{anime.synopsis}</p>
+                <ul>
+                  {anime.genres.map((genre) => (
+                    <li key={genre.mal_id}>
+                      {genre.name} <BsDot className="dot__svg" />
+                    </li>
+                  ))}
+                  {anime.themes.map((theme) => (
+                    <li key={theme.mal_id}>
+                      {theme.name}
+                      <BsDot className="dot__svg" />
+                    </li>
+                  ))}
+                  {anime.demographics.map((demo) => (
+                    <li key={demo.mal_id}>
+                      {demo.name}
+                      <BsDot className="dot__svg" />
+                    </li>
+                  ))}
+                </ul>
+                <p>{anime.synopsis}</p>
 
-              <div>
-                <div>
-                  <p>Score</p>
-                  <p>{SCORE}&#37;</p>
-                </div>
+                <AnimeShowcase>
+                  <div>
+                    {anime.score ? (
+                      <>
+                        <p>Score</p>
+                        <p>{SCORE}&#37;</p>
+                      </>
+                    ) : (
+                      <>
+                        <p>Score</p>
+                        <p>No result yet</p>
+                      </>
+                    )}
+                  </div>
 
-                <div>
-                  <p>Total episodes</p>
-                  <p>{anime.episodes}</p>
-                </div>
-                <div>
-                  <p>Duration</p>
-                  <p>{DURATION} minutes</p>
-                </div>
-                <div>
-                  <p>Status</p>
-                  <p>{anime.status}</p>
-                </div>
-              </div>
-            </div>
-          </Section>
-        </div>
+                  <div>
+                    {anime.episodes ? (
+                      <>
+                        <p>Total episodes</p>
+                        <p>{anime.episodes} </p>
+                      </>
+                    ) : (
+                      <>
+                        <p>Total episodes</p>
+                        <p>No result yet</p>
+                      </>
+                    )}
+                  </div>
+                  <div>
+                    {anime.duration ? (
+                      <>
+                        <p>Duration</p>
+                        <p>{DURATION} minutes</p>
+                      </>
+                    ) : (
+                      <>
+                        <p>Duration</p>
+                        <p>No result yet</p>
+                      </>
+                    )}
+                  </div>
+                  <div>
+                    {anime.status ? (
+                      <>
+                        <p>Status</p>
+                        <p>{anime.status}</p>
+                      </>
+                    ) : (
+                      <>
+                        <p>Status</p>
+                        <p>No result yet</p>
+                      </>
+                    )}
+                  </div>
+                </AnimeShowcase>
+              </AnimeInfo>
+            </Section>
+          </div>
+        </>
       ) : (
         ''
       )}
---
+++ src/pages/Anime/style.tsx
@@ -0,0 +1,122 @@
+import styled from '@emotion/styled'
+import { theme } from '../../styles/theme'
+
+export const Banner = styled.div`
+  height: 40vh;
+  margin: auto;
+  display: flex;
+  position: relative;
+  align-items: center;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  transition: 0.3s ease;
+
+  :hover .play__link {
+    opacity: 1;
+  }
+  @media screen and (max-width: 748px) {
+    display: none;
+  }
+`
+
+export const Section = styled.div`
+  margin: auto;
+  display: flex;
+  padding: 0 30px;
+  max-width: 2200px;
+`
+
+export const Poster = styled.img`
+  width: 20rem;
+  height: 25rem;
+  transform: translateY(-80px);
+
+  /* @media screen and (max-width: 1500px) {
+    width: 11rem;
+    height: 18rem;
+  } */
+`
+
+export const Button = styled.button`
+  cursor: pointer;
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  color: ${theme.White};
+  background-color: ${theme.Primary};
+  padding: 10px;
+  width: 150px;
+  border: none;
+  border-radius: 4px;
+  transform: translateY(-50px);
+  transition: all 0.2s ease;
+
+  .play__svg {
+    font-size: calc(20 / 16 * 1rem);
+    margin-right: 5px;
+  }
+
+  :hover {
+    background-color: ${theme.Secondary};
+  }
+`
+
+export const AnimeInfo = styled.div`
+  padding: 0 30px;
+
+  h1 {
+    margin-top: -30px;
+    padding-bottom: 15px;
+    font-size: calc(30 / 16 * 1rem);
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    font-size: 1rem;
+  }
+
+  li .dot__svg {
+    font-size: calc(20 / 16 * 1rem);
+  }
+
+  li:last-child .dot__svg {
+    display: none;
+  }
+
+  p:first-of-type {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    margin-top: 5px;
+    color: ${theme.Grey};
+  }
+`
+
+export const AnimeShowcase = styled.div`
+  display: flex;
+  padding-top: 30px;
+  align-items: center;
+
+  div {
+    p {
+      font-size: 1rem;
+    }
+
+    p:first-of-type {
+      color: #9ca3af;
+      font-weight: 700;
+      padding-bottom: 5px;
+    }
+
+    p:last-child {
+      font-weight: 500;
+      color: #9ca3af;
+    }
+  }
+
+  div:not(:last-child) {
+    margin-right: 60px;
+  }
+`
