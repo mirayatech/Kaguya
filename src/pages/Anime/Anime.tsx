fb7f359384f9e28b145722ac39dc82c8cfb1a6f5
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-05 16:31:05 +0100

    feat: fetch spesific anime

--- src/pages/Anime/Anime.tsx
+++ src/pages/Anime/Anime.tsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AnimeType } from '../../library'
+import { Banner } from './style'
+
 export default function Anime() {
-  return <div>Anime</div>
+  const [anime, setAnime] = useState<AnimeType | null>(null)
+
+  const { id } = useParams()
+
+  const URL = `https://api.jikan.moe/v4/anime/${id}/full`
+
+  const getAnimeInformation = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    console.log(data.data)
+    setAnime(data.data)
+  }
+
+  useEffect(() => {
+    getAnimeInformation()
+  }, [])
+
+  return <></>
 }
